c7a334dd75976ebb24f2595439177f49f55d7fe2
Renato Goedert <renato.goedert@waltoninstitute.ie>
2025-03-28 12:41:26 +0000

    Initial Chat Screen and testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,119 +8,144 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Messaging App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const ChatScreen(title: "Chat"),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<ChatScreen> createState() => _ChatScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _messageControler = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  List<Map<String, dynamic>> messages = [
+    {
+      'sender': 'Alice',
+      'text': 'Hey, how are you?',
+      'time': '10:30 AM',
+      'isMe': false,
+    },
+    {
+      'sender': 'Me',
+      'text': 'I\'m good, thanks! How about you?',
+      'time': '10:32 AM',
+      'isMe': true,
+    },
+    // Additional messages can be added here
+  ];
+
+  void _sendMessage() {
+    if (kDebugMode) {
+      print('send message function run');
+      print('Message text to be added: ${_messageControler.text}');
+    }
+    if (_messageControler.text.isNotEmpty) {
+      setState(() {
+        messages.add({
+          'sender': 'Me',
+          'text': _messageControler.text,
+          'time': TimeOfDay.now().format(context),
+          'isMe': true,
+        });
+        if (kDebugMode) {
+          print("Message Added!");
+        }
+        _messageControler.clear();
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Column(
+          children: [
+            //Message Log
+            Expanded(
+                child: Container(
+              padding: EdgeInsets.all(10),
+              child: ListView.builder(
+                  itemCount: messages.length,
+                  itemBuilder: (context, index) {
+                    final message = messages[index];
+
+                    return Align(
+                        alignment: message['isMe']
+                            ? Alignment.centerRight
+                            : Alignment.centerLeft,
+                        child: Container(
+                          margin: const EdgeInsets.symmetric(vertical: 5),
+                          padding: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                              color: message['isMe']
+                                  ? Colors.blue[100]
+                                  : Colors.grey[300],
+                              borderRadius: BorderRadius.circular(10)),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                message['text'],
+                                style: const TextStyle(fontSize: 16),
+                              ),
+                              const SizedBox(height: 5),
+                              Text(
+                                message['time'],
+                                style: TextStyle(
+                                    fontSize: 12, color: Colors.grey[600]),
+                              )
+                            ],
+                          ),
+                        ));
+                  }),
+            )),
+
+            // //Message input Field
+            Container(
+              padding:
+                  EdgeInsets.only(top: 10, right: 10, left: 10, bottom: 25),
+              color: Colors.white,
+              child: Row(
+                children: [
+                  Expanded(
+                      child: TextField(
+                    controller: _messageControler,
+                    decoration: InputDecoration(
+                        hintText: 'Type a message',
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20))),
+                  )),
+                  IconButton(
+                      icon: const Icon(Icons.send),
+                      onPressed: () {
+                        if (kDebugMode) {
+                          print('send cliked!');
+                        }
+                        _sendMessage();
+                      })
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,34 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:fluter_rust_message_app/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('ChatScreen displays initial messages and UI components',
+      (WidgetTester tester) async {
+    // Build the ChatScreen widget
+    await tester.pumpWidget(const MaterialApp(home: ChatScreen(title: 'Chat')));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verify the presence of initial messages
+    expect(find.text('Hey, how are you?'), findsOneWidget);
+    expect(find.text('I\'m good, thanks! How about you?'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    // Verify the presence of the message input field and send button
+    expect(find.byType(TextField), findsOneWidget);
+    expect(find.byIcon(Icons.send), findsOneWidget);
+  });
+
+  testWidgets('Sending a new message updates the chat log',
+      (WidgetTester tester) async {
+    // Build the ChatScreen widget
+    await tester.pumpWidget(const MaterialApp(home: ChatScreen(title: 'Chat')));
+
+    // Enter text into the message input field
+    await tester.enterText(
+        find.byType(TextField), 'Hello, this is a test message.');
+
+    // Tap the send button
+    await tester.tap(find.byIcon(Icons.send));
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify the new message appears in the chat log
+    expect(find.text('Hello, this is a test message.'), findsOneWidget);
   });
 }
